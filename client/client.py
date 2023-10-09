2e6d7157b9822ea7e91e1dd3839d92f458d2aad7
CIANDT\lucas.matheus <lucas.matheu@ciandt.com>
2023-10-09 19:42:01 -0300

    feat: finalizando projeto

--- client/client.py
+++ client/client.py
@@ -18,9 +18,11 @@ daemon = Pyro5.api.Daemon()
 @Pyro5.api.expose
 class Client(object):
     
-    def notification(self,notification): 
-        print(notification)
-        
+    def notification(self,notification):
+        print("--Notificação--") 
+        for detail in notification:
+            print(detail)
+        print("--------")
 
 def acess_server():
     nameserver = Pyro5.api.locate_ns()
@@ -47,7 +49,7 @@ def create_new_client(name):
     
 ## Funções de exibição de menus
 def printMenu():
-    print("\n--------------------------------")
+    print("--------------------------------")
     print("          MENU         \n")
     print("[1] - Cadastrar Novo produto")
     print("[2] - Retirar produto")
@@ -64,18 +66,32 @@ def printMenu():
         exit()
     printMenu()
 
+def return_menu():
+    option = input("\nDeseja voltar ao menu S/n (S)\n").strip()
+    if option == 'S' or option == 's':
+        printMenuRelatorio()
+    elif option == 'N' or "n":
+        exit()
+    else:
+        return_menu()
+
 def printMenuRelatorio():
     print("\n          MENU RELATORIO        \n")
     print("[1] - Visualizar estoque")
     print("[2] - Entrada e Saida")
     print("[3] - Produtos sem saida")
+    print("[4] - Voltar")
     option = input("Digite a ação que deseja realizar\n").strip()
     if option in [1,"1"]:
         show_stock()
-    elif option [2,"2"]:
-        input("Digite o periodo de visualização em minutos\n").strip()
-    elif option [3,"3"]:
-        input("Digite o periodo de visualização em minutos\n").strip()
+    elif option == "2":
+        period = input("Digite o periodo de visualização em minutos\n").strip()
+        show_input_output_stock(period)
+    elif option == "3":
+      period = input("Digite o periodo de visualização em minutos\n").strip()
+      show_products_without_stock_period(period)
+    elif option == "4":
+        printMenu()
 
 ## --------
 
@@ -89,30 +105,44 @@ def addProduct():
     price = input("Qual o preço do produto:\n").strip()
     minimalStorage = input("Qual a quantidade minima do produto:\n").strip()
     produto = Product(code,name,description,amount,price,minimalStorage)
-    result = server.create_product(json.dumps(produto.__dict__))
+    result = server.create_product(json.dumps(produto.__dict__),getSignatureToKey(gestor.name))
     print(result)
 
 def getProduct():
+    global gestor
     code = input("Qual o codigo do produto:").strip()
     amount = input("Qual a quantidade do produto sera retirada:").strip()
     result = server.get_product(code,amount,getSignatureToKey(gestor.name))
     print(result)
+
+## ---------
+
+## Relatorios de pedido
 
 def show_stock():
     stock = server.show_stock()
     print("Estoque de produtos:")    
     for obj in stock:
         print(obj)
-    option = input("\nDeseja voltar ao menu S/n (S)\n").strip()
-    if option == 'S' or option == 's':
-        printMenuRelatorio()
-    else:
-        exit()
+    return_menu()
+
+def show_input_output_stock(period):
+    stock = server.show_input_output_stock(period)
+    for obj in stock:
+        print(obj)
+    return_menu()
+
+def  show_products_without_stock_period(period):
+    stock = server.show_products_without_stock_period(period) 
+    for obj in stock:
+        print(obj)
+    return_menu()
+
 ## ------------------
 
 def init_user():
     global gestor
-    name = input("Digite o nome do gestor que esta acessando o Sistema\n").strip()
+    name = input("\nDigite o nome do gestor que esta acessando o Sistema\n").strip()
     gestor = create_new_client(name)
     server.create_client(json.dumps(gestor.__dict__))
     print("Gestor acessado com sucesso \n")
